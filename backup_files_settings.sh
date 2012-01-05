172b264bde4c1db3652f47b503d19aeb873891ca
Petter Blomberg <github@petterblomberg.se>
2012-01-05 22:10:06 +0100

    Completion of last commit - english variable names

--- backup_files_settings.sh
+++ backup_files_settings.sh
@@ -3,7 +3,7 @@
 # Create strings
 echo HOSTNAME=`hostname`
 echo DAY_OF_WEEK=`date +%A`
-echo KATALOGER=\" /home /root /var/www /etc \"
+echo BACKUP_THIS=\" /home /root /var/www /etc \"
 echo TARGET='please.change.this.hostname'
 
 echo TMPDIR='/home/backupuser' # Must be absolute
